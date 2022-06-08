7bf85f596690226ec1ce58138eb99f92ee2c6917
Graham Walker <gwalker256@gmail.com>
2022-06-08 16:00:37 +0100

    day3act1

---
+++ day3act.py
@@ -0,0 +1,50 @@
+print ("hello")
+
+# countries = {"United Kingdom":"London", "France": "Paris", "Germany": "Berlin", "Spain": "Madrid","Italy": "Rome"}
+# countries.setdefault("Ireland", "Dublin")
+# countries.setdefault("Portugal", "Lisbon")
+
+# print(countries)
+
+# countries["United Kingdom"] = "English"
+
+# print(countries)
+
+
+# print(complex_dict["one"]["name"])
+
+# complex_dict = {
+#     "one" : {
+#     "name": "Will",
+#     "age": 31,
+#     "instructor": True
+# }, "two" : {
+#     "name": "Jay",
+#     "age": "30",
+#     "instructor": True
+# }}
+
+# print(complex_dict["one"]["name"])
+
+# countries = {
+#     "UK" : {
+#     "name": "United Kingdom",
+#     "capital": "London"
+    
+#     }, "FR" : {
+#         "name": "France",
+#         "capital": "Paris"
+#     }, "UK" : {
+#         "name": "United Kingdom",
+#         "capital": "London"
+#     }, "GE" : {
+#         "name": "Germany",
+#         "capital": "Berlin"
+#     }, "SP" : {
+#         "name": "Spain",
+#         "capital": "Madrid"
+#         }, "IT" : {
+#         "name": "Italy",
+#         "capital": "Rome"
+# }
+# }
---
+++ dict.py
@@ -0,0 +1,39 @@
+print("hi")
+
+# my_cat = {"name":"Salem", "colour":"black", "mood":"sassy"}
+
+# print (my_cat["name"])
+
+# for i in my_cat.values():
+#     print(i)
+
+# for i in my_cat.keys():
+#     print(i)
+
+# for i in my_cat.items():
+#     print(i)
+
+# my_cat["name"] = "Barry"
+
+# print(my_cat["name"])
+
+# complex_dict = {
+#     "one" : {
+#     "name": "Will",
+#     "age": 31,
+#     "instructor": True
+# }, "two" : {
+#     "name": "Jay",
+#     "age": "30",
+#     "instructor": True
+# }}
+
+# print(complex_dict["one"]["name"])
+
+my_pet = {"species":"malayan tapir", "name":"fluffy", "sex":"female", "age":"8"}
+
+print(my_pet)
+
+my_pet["age"] = "9"
+
+print(my_pet["name"] + " is now " + my_pet["age"])
